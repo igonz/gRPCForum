212a6947f6ce5675fdfc2223b806c20de5433ba8
IsmaelG <ismaelg@netflix.com>
2019-02-04 18:37:19 -0800

    Initial Commit

---
+++ src/main/java/com/sample/forum/gRPC/Forum/GRpcForumApplication.java
@@ -0,0 +1,14 @@
+package com.sample.forum.gRPC.Forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GRpcForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GRpcForumApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/sample/forum/gRPC/Forum/GRpcForumApplicationTests.java
@@ -0,0 +1,17 @@
+package com.sample.forum.gRPC.Forum;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GRpcForumApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
